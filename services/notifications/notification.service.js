b5204efd5b5a1fef7c4e806883542c0a4680e5b9
Turentu <todaysalento@gmail.com>
2026-06-07 15:00:02 +0200

    cache

--- services/notifications/notification.service.js
+++ services/notifications/notification.service.js
@@ -1,5 +1,5 @@
 import { pool } from '../../db/db.js';
-import { sendPush } from './pushService.js'; 
+import { sendPush } from './push.service.js'; 
 import { getIO } from '../../socket.js';
 
 /**
